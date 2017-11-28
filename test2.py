50ceff62b2b474cb028f60f200445f12ea3d8a98
[bonnettd] <[bonnettd@allegheny.edu]>
2017-11-28 00:19:01 -0500

    Second commit with atom

--- test2.py
+++ test2.py
@@ -12,9 +12,18 @@ def print_backwards(myList):
 def print_ascending(myList):
     ascended_list = sorted(myList, key = int)
     print("The ascending list is " + repr(ascended_list))
+    return 0
+
+
+"""Print the elements of a list in descending order"""
+def print_descending(myList):
+    descended_list = sorted(myList, key = int, reverse = True)
+    print("The descended list is " + repr(descended_list))
+    return 0
 
 
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 list2 = [4, 6, 3, 5, 1, 9, 2]
 print_backwards(list1)
 print_ascending(list2)
+print_descending(list2)
